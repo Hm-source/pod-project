fac4ae19390514c46ef1497eb2b74a2334c94ba7
Hm-source <swo8852@gmail.com>
2025-08-27 14:17:15 +0900

    init: project setting

---
+++ src/test/java/org/example/podcommerce/PodCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.podcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
